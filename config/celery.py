0aaba1541b22267cf57455316c14ecf66d1612b6
Alena <alittlekindness94@gmail.com>
2025-05-21 17:43:13 +0500

    dz

--- config/celery.py
+++ config/celery.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
+from datetime import timedelta
+
 from celery import Celery
 from celery.schedules import crontab
 
@@ -9,5 +11,11 @@ app = Celery('config')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+CELERY_BEAT_SCHEDULE = {
+    'task-name': {
+        'task': 'config.tasks.my_task',
+        'schedule': timedelta(minutes=10),
+    },
+}
 
 app.autodiscover_tasks()
